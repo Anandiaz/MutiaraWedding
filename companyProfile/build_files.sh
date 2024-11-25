fa8bf61e6494bea854e83ba3577c4a8d9ea3fc32
Anandiaz <anandiazagung@gmail.com>
2024-11-25 18:36:11 +0800

    Feat: Setup for Vercel Wish Me luck

--- companyProfile/build_files.sh
+++ companyProfile/build_files.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-echo "Starting build process..."
-
-echo "Installing dependencies..."
 pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3.9 manage.py collectstatic --noinput --clear
-
-echo "Build process completed."
+python3.9 manage.py collectstatic 
