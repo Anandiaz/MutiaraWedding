d1197b904230002ff54e15ac48f1f5639b97a6c3
Anandiaz <anandiazagung@gmail.com>
2024-11-25 17:16:50 +0800

    Feat: Setup for Vercel Wish Me luck

--- companyProfile/companyProfile/settings.py
+++ companyProfile/companyProfile/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-5+lg5%tuna@t-)-1b_24r8rbmxlbv)hk0jt@hox4n)n9joml0w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
