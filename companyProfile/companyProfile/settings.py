b3ab50345619e12be12992268ace489a1f245028
Anandiaz <anandiazagung@gmail.com>
2024-11-25 20:36:18 +0800

    Feat: Setup for Vercel Wish Me luck

--- companyProfile/companyProfile/settings.py
+++ companyProfile/companyProfile/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Directory where static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Additional directories to look for static files during development
 STATICFILES_DIRS = [
