5e5b08ab6bb05d0dfd69260145c66c569a33c177
Anandiaz <anandiazagung@gmail.com>
2024-11-25 20:25:22 +0800

    Feat: Setup for Vercel Wish Me luck

--- companyProfile/companyProfile/settings.py
+++ companyProfile/companyProfile/settings.py
@@ -133,13 +133,10 @@ USE_TZ = True
 STATIC_URL = "static/"
 
 # Directory where static files will be collected
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Additional directories to look for static files during development
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Your custom static files
-    os.path.join(BASE_DIR, 'theme/static'),  # Include your Tailwind theme static files if needed
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
